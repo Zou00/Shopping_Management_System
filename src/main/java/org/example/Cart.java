01c35f975ee675336ee2a34b891692990026e8dc
Zou00 00 <1064562993@qq.com>
2023-08-29 07:10:44 +0000

    ADD

--- src/main/java/org/example/Cart.java
+++ src/main/java/org/example/Cart.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Cart {
@@ -16,34 +11,33 @@ public class Cart {
     //查看商品
     public void listProducts() {
         System.out.println("---------商品信息---------");
-        //商品编号、商品名称、生产厂家、生产日期、型号、零售价格
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\ShoppingSystem\\products.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                if (userInfo.length >= 4) {
-                    String productId = userInfo[0];
-                    String productname = userInfo[1];
-                    String origin = userInfo[2];
-                    String dateTime = userInfo[3];
-                    String size = userInfo[4];
-                    double outprice = Double.parseDouble(userInfo[6]);
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0);
 
-                    System.out.println("商品编号：" + productId);
-                    System.out.println("商品名称：" + productname);
-                    System.out.println("生产厂家：" + origin);
-                    System.out.println("生产日期：" + dateTime);
-                    System.out.println("型号：" + size);
-                    System.out.println("零售价格：" + outprice);
-                    System.out.println("-------------------------");
-                }
+            for (Row row : sheet) {
+                String productId = row.getCell(0).getStringCellValue();
+                String productname = row.getCell(1).getStringCellValue();
+                String origin = row.getCell(2).getStringCellValue();
+                String dateTime = row.getCell(3).getStringCellValue();
+                String size = row.getCell(4).getStringCellValue();
+                double outprice = row.getCell(6).getNumericCellValue();
+
+                System.out.println("商品编号：" + productId);
+                System.out.println("商品名称：" + productname);
+                System.out.println("生产厂家：" + origin);
+                System.out.println("生产日期：" + dateTime);
+                System.out.println("型号：" + size);
+                System.out.println("零售价格：" + outprice);
+                System.out.println("-------------------------");
             }
         } catch (IOException e) {
             System.out.println("读取商品信息时出错：" + e.getMessage());
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
         }
     }
 
-    //加入购物车
+    // 加入购物车
     public void addToCart() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("请输入商品编号：");
@@ -51,101 +45,96 @@ public class Cart {
         System.out.print("请输入数量：");
         int num = scanner.nextInt();
 
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设购物车信息在第一个Sheet中
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Programming\\ShoppingSystem\\cart.txt", true))) {
-            writer.write(productID + " " + num);
-            writer.newLine();
+            int rowCount = sheet.getLastRowNum(); // 获取行数
+
+            Row row = sheet.createRow(rowCount + 1); // 创建新行
+            Cell productIdCell = row.createCell(0); // 商品编号列
+            Cell numCell = row.createCell(1); // 数量列
+
+            productIdCell.setCellValue(productID);
+            numCell.setCellValue(num);
+
+            FileOutputStream fileOut = new FileOutputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx");
+            workbook.write(fileOut);
+            fileOut.close();
+
             System.out.println("添加成功！");
         } catch (IOException e) {
             System.out.println("保存商品信息时出错：" + e.getMessage());
         }
     }
 
-    //移除商品
+    // 移除商品
     public void removeFromCart(String id) {
-        File file = new File("D:\\Programming\\ShoppingSystem\\cart.txt");
-        File tempFile = new File("D:\\Programming\\ShoppingSystem\\tempCart.txt");
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设购物车信息在第一个Sheet中
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                String productId = userInfo[0];
-                if (productId.equals(id)) {
-                    continue;
+            for (Row row : sheet) {
+                Cell cell = row.getCell(0); // 商品编号列
+                if (cell != null && cell.getStringCellValue().equals(id)) {
+                    sheet.removeRow(row);
+                    break;
                 }
-                writer.write(line);
-                writer.newLine();
             }
 
+            FileOutputStream fileOut = new FileOutputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx");
+            workbook.write(fileOut);
+            fileOut.close();
+
+            System.out.println("移除购物车成功！");
         } catch (IOException e) {
             System.out.println("执行删除操作时出错：" + e.getMessage());
-            return;
-        }
-
-        // 删除原始文件
-        if (file.delete()) {
-            // 重命名临时文件为原始文件名
-            if (!tempFile.renameTo(file)) {
-                System.out.println("重命名文件时出错");
-            } else {
-                System.out.println("移除购物车成功！");
-            }
-        } else {
-            System.out.println("删除原始文件时出错");
         }
     }
 
-    //修改商品
+    // 修改商品
     public void modifyCart(String id) {
-        List<String> lines = new ArrayList<>();
-        scanner.nextLine();
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\ShoppingSystem\\cart.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                String productId = userInfo[0];
-                if (id.equals(productId)) {
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设购物车信息在第一个Sheet中
+
+            for (Row row : sheet) {
+                Cell cell = row.getCell(0); // 商品编号列
+                if (cell != null && cell.getStringCellValue().equals(id)) {
                     System.out.print("请输入新的数量: ");
                     double num = scanner.nextDouble();
-                    userInfo[1] = String.valueOf(num);
-                    line = String.join(" ", userInfo);
+                    Cell numCell = row.getCell(1); // 数量列
+                    numCell.setCellValue(num);
+
+                    FileOutputStream fileOut = new FileOutputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx");
+                    workbook.write(fileOut);
+                    fileOut.close();
+
                     System.out.println("已修改数量！");
+                    return;
                 }
-                lines.add(line);
             }
+
+            System.out.println("未找到要修改的商品编号！");
         } catch (IOException e) {
             System.out.println("读取商品信息时出错：" + e.getMessage());
-            return;
-        }
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Programming\\ShoppingSystem\\cart.txt"))) {
-            for (String line : lines) {
-                writer.write(line);
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("保存商品信息时出错：" + e.getMessage());
         }
     }
 
-    //查看购物车
+    // 查看购物车
     public void list() {
         System.out.println("---------购物车---------");
-        //商品编号、零售价格
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\ShoppingSystem\\cart.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                String productId = userInfo[0];
-                int num = Integer.parseInt(userInfo[1]);
+
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream("D:\\Programming\\ShoppingSystem\\cart.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设购物车信息在第一个Sheet中
+
+            for (Row row : sheet) {
+                Cell productIdCell = row.getCell(0); // 商品编号列
+                Cell numCell = row.getCell(1); // 数量列
+
+                String productId = productIdCell.getStringCellValue();
+                double num = numCell.getNumericCellValue();
 
                 System.out.println("商品编号：" + productId);
                 System.out.println("数量：" + num);
                 System.out.println("-------------------------");
-
             }
         } catch (IOException e) {
             System.out.println("读取商品信息时出错：" + e.getMessage());
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,37 +10,36 @@ import java.util.Scanner;
 public class Product {
     Scanner scanner = new Scanner(System.in);
 
-    //列出所有商品信息
+    //列出商品信息
     public void listProducts() {
         System.out.println("---------商品信息---------");
-        //商品编号、商品名称、生产厂家、生产日期、型号、进货价、零售价格、数量
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\ShoppingSystem\\products.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                if (userInfo.length >= 4) {
-                    String productId = userInfo[0];
-                    String productname = userInfo[1];
-                    String origin = userInfo[2];
-                    String dateTime = userInfo[3];
-                    String size = userInfo[4];
-                    double inprice = Double.parseDouble(userInfo[5]);
-                    double outprice =Double.parseDouble(userInfo[6]) ;
-                    int num = Integer.parseInt(userInfo[7]);
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设商品信息在第一个Sheet中
 
-                    System.out.println("商品编号：" + productId);
-                    System.out.println("商品名称：" + productname);
-                    System.out.println("生产厂家：" + origin);
-                    System.out.println("生产日期：" + dateTime);
-                    System.out.println("型号：" + size);
-                    System.out.println("进价：" + inprice);
-                    System.out.println("零售价格：" + outprice);
-                    System.out.println("数量：" + num);
-                    System.out.println("-------------------------");
-                }
+            for (Row row : sheet) {
+                String productId = row.getCell(0).getStringCellValue();
+                String productname = row.getCell(1).getStringCellValue();
+                String origin = row.getCell(2).getStringCellValue();
+                String dateTime = row.getCell(3).getStringCellValue();
+                String size = row.getCell(4).getStringCellValue();
+                double inprice = row.getCell(5).getNumericCellValue();
+                double outprice = row.getCell(6).getNumericCellValue();
+                int num = (int) row.getCell(7).getNumericCellValue();
+
+                System.out.println("商品编号：" + productId);
+                System.out.println("商品名称：" + productname);
+                System.out.println("生产厂家：" + origin);
+                System.out.println("生产日期：" + dateTime);
+                System.out.println("型号：" + size);
+                System.out.println("进价：" + inprice);
+                System.out.println("零售价格：" + outprice);
+                System.out.println("数量：" + num);
+                System.out.println("-------------------------");
             }
         } catch (IOException e) {
             System.out.println("读取商品信息时出错：" + e.getMessage());
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
         }
     }
 
@@ -67,123 +63,161 @@ public class Product {
         System.out.print("请输入数量：");
         int num = scanner.nextInt();
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Programming\\ShoppingSystem\\products.txt", true))) {
-            writer.write(productID + " " + productname + " " + origin + " " + datetime + " " + size + " " + inprice + " " + outprice + " " + num);
-            writer.newLine();
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"));
+             FileOutputStream fileOut = new FileOutputStream("D:\\Programming\\ShoppingSystem\\products.xlsx")) {
+
+            Sheet sheet = workbook.getSheetAt(0); // 假设商品信息在第一个Sheet中
+            int rowCount = sheet.getLastRowNum();
+
+            Row newRow = sheet.createRow(rowCount + 1);
+            newRow.createCell(0).setCellValue(productID);
+            newRow.createCell(1).setCellValue(productname);
+            newRow.createCell(2).setCellValue(origin);
+            newRow.createCell(3).setCellValue(datetime);
+            newRow.createCell(4).setCellValue(size);
+            newRow.createCell(5).setCellValue(inprice);
+            newRow.createCell(6).setCellValue(outprice);
+            newRow.createCell(7).setCellValue(num);
+
+            workbook.write(fileOut);
             System.out.println("添加成功！");
         } catch (IOException e) {
             System.out.println("保存商品信息时出错：" + e.getMessage());
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
         }
     }
 
     //修改商品信息
     public void modifyProduct(String id) {
-        List<String> lines = new ArrayList<>();
+        List<String[]> rows = new ArrayList<>();
         scanner.nextLine();
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\ShoppingSystem\\products.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                if (userInfo.length >= 4) {
-                    String productId = userInfo[0];
-                    if (id.equals(productId)) {
-                        System.out.print("请输入新的零售价格: ");
-                        double newprice = scanner.nextDouble();
-                        userInfo[6] = String.valueOf(newprice);
-                        line = String.join(" ", userInfo);
-                        System.out.println("已重置零售价格！");
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设商品信息在第一个Sheet中
+
+            for (Row row : sheet) {
+                String productId = row.getCell(0).getStringCellValue();
+
+                if (id.equals(productId)) {
+                    System.out.print("请输入新的零售价格: ");
+                    double newprice = scanner.nextDouble();
+                    row.getCell(6).setCellValue(newprice);
+                    System.out.println("已重置零售价格！");
+                }
+
+                String[] line = new String[8];
+                for (int i = 0; i < 8; i++) {
+                    Cell cell = row.getCell(i);
+                    if (cell != null) {
+                        if (cell.getCellType() == CellType.NUMERIC) {
+                            line[i] = String.valueOf(cell.getNumericCellValue());
+                        } else {
+                            line[i] = cell.getStringCellValue();
+                        }
+                    } else {
+                        line[i] = "";
                     }
                 }
-                lines.add(line);
+                rows.add(line);
             }
         } catch (IOException e) {
             System.out.println("读取商品信息时出错：" + e.getMessage());
             return;
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
+            return;
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Programming\\ShoppingSystem\\products.txt"))) {
-            for (String line : lines) {
-                writer.write(line);
-                writer.newLine();
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"));
+             FileOutputStream fileOut = new FileOutputStream("D:\\Programming\\ShoppingSystem\\products.xlsx")) {
+
+            Sheet sheet = workbook.getSheetAt(0); // 假设商品信息在第一个Sheet中
+
+            for (int i = 0; i < rows.size(); i++) {
+                Row row = sheet.getRow(i);
+                String[] line = rows.get(i);
+                for (int j = 0; j < line.length; j++) {
+                    Cell cell = row.getCell(j);
+                    if (cell != null) {
+                        if (cell.getCellType() == CellType.NUMERIC) {
+                            double value = Double.parseDouble(line[j]);
+                            cell.setCellValue(value);
+                        } else {
+                            cell.setCellValue(line[j]);
+                        }
+                    }
+                }
             }
+
+            workbook.write(fileOut);
         } catch (IOException e) {
             System.out.println("保存商品信息时出错：" + e.getMessage());
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
         }
     }
 
     //删除商品信息
     public void deleteProduct(String id) {
-        File file=new File("D:\\Programming\\ShoppingSystem\\products.txt");
-        File tempFile=new File("D:\\Programming\\ShoppingSystem\\tempProducts.txt");
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"));
+             FileOutputStream fileOut = new FileOutputStream("D:\\Programming\\ShoppingSystem\\products.xlsx")) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设商品信息在第一个Sheet中
+            Iterator<Row> iterator = sheet.iterator();
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                if (userInfo.length >= 4) {
-                    String productId = userInfo[0];
+            while (iterator.hasNext()) {
+                Row row = iterator.next();
+                Cell cell = row.getCell(0);
+                if (cell != null && cell.getCellType() == CellType.STRING) {
+                    String productId = cell.getStringCellValue();
                     if (productId.equals(id)) {
-                        continue;
+                        iterator.remove();
+                        System.out.println("删除商品信息成功！");
                     }
                 }
-                writer.write(line);
-                writer.newLine();
             }
 
+            workbook.write(fileOut);
         } catch (IOException e) {
             System.out.println("执行删除操作时出错：" + e.getMessage());
-            return;
-        }
-
-        // 删除原始文件
-        if (file.delete()) {
-            // 重命名临时文件为原始文件名
-            if (!tempFile.renameTo(file)) {
-                System.out.println("重命名文件时出错");
-            } else {
-                System.out.println("删除商品信息成功！");
-            }
-        } else {
-            System.out.println("删除原始文件时出错");
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
         }
     }
 
     //查询商品信息
-    public void checkProduct(String checkname,String productid){
+    public void checkProduct(String checkname, String productid) {
         System.out.println("-----------------------");
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\ShoppingSystem\\products.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(" ");
-                if (userInfo.length >= 4) {
-                    String productId = userInfo[0];
-                    String productname = userInfo[1];
-                    String origin=userInfo[2];
-                    String dateTime = userInfo[3];
-                    String size=userInfo[4];
-                    double inprice=Double.parseDouble(userInfo[5]);
-                    double outprice=Double.parseDouble(userInfo[6]);
-                    int num=Integer.parseInt(userInfo[7]);
+        try (Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\Programming\\ShoppingSystem\\products.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0); // 假设商品信息在第一个Sheet中
 
-                    if ((productid.equals(productId)) || (productname.equals(checkname))) {
-                        System.out.println("商品编号：" + productId);
-                        System.out.println("商品名称：" + productname);
-                        System.out.println("生产厂家："+origin);
-                        System.out.println("生产日期：" +dateTime);
-                        System.out.println("型号："+size);
-                        System.out.println("进价："+inprice);
-                        System.out.println("零售价格："+outprice);
-                        System.out.println("数量："+num);
-                        System.out.println("-------------------------");
-                    }else {
-                        System.out.print(" ");
-                    }
+            for (Row row : sheet) {
+                String productId = row.getCell(0).getStringCellValue();
+                String productname = row.getCell(1).getStringCellValue();
+
+                if ((productid.equals(productId)) || (productname.equals(checkname))) {
+                    String origin = row.getCell(2).getStringCellValue();
+                    String dateTime = row.getCell(3).getStringCellValue();
+                    String size = row.getCell(4).getStringCellValue();
+                    double inprice = row.getCell(5).getNumericCellValue();
+                    double outprice = row.getCell(6).getNumericCellValue();
+                    int num = (int) row.getCell(7).getNumericCellValue();
+
+                    System.out.println("商品编号：" + productId);
+                    System.out.println("商品名称：" + productname);
+                    System.out.println("生产厂家：" + origin);
+                    System.out.println("生产日期：" + dateTime);
+                    System.out.println("型号：" + size);
+                    System.out.println("进价：" + inprice);
+                    System.out.println("零售价格：" + outprice);
+                    System.out.println("数量：" + num);
+                    System.out.println("-------------------------");
                 }
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("读取商品信息时出错：" + e.getMessage());
+        } catch (InvalidFormatException e) {
+            System.out.println("非法的Excel文件格式：" + e.getMessage());
         }
     }
-}
+
